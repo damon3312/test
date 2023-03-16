077e099d5cc3c70eac31e116537c198663bee888
jjasici1 <ec21744@qmul.ac.uk>
2023-03-16 14:33:28 +0000

    message

--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -19,6 +19,8 @@ export default class Iphone extends Component {
 			description: "",
 			display: true,
 			display2: true,
+			home:true,
+			dpage: false,
 			disruptions:[]
 		};
 	}
@@ -26,7 +28,7 @@ export default class Iphone extends Component {
 	// a call to fetch weather data via openweathermap
 	fetchWeatherData = () => {
 		// API URL with a structure of: http://api.openweathermap.org/data/2.5/weather?q=city,country&APPID=apikey
-		var url = "http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=0b5ada9e11785005df4e2b993928203c";
+		var url = "http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=776b7cbe36ab4e4ce2cd647150265193";
 		$.ajax({
 			url: url,
 			dataType: "jsonp",
@@ -40,7 +42,7 @@ export default class Iphone extends Component {
 	fetchDisruptionData = () => {
 		const lines = ["northern", "central", "circle", "district", "jubilee", "metropolitan", "northern", "piccadilly", "victoria", "bakerloo"];
 		const promises = lines.map(line => {
-		  const url = `https://api.tfl.gov.uk/Line/${line}/Disruption`;
+		  const url = `https://api.tfl.gov.uk/Line/${lines}/Disruption`;
 		  return $.ajax({
 			url: url,
 			dataType: "json",
@@ -58,41 +60,49 @@ export default class Iphone extends Component {
 		  console.log('API call failed ' + error);
 		});
 		this.setState({ display2: false });
-	  }
-
-	fetch4HourWeather = () =>{
-	    var url ="https://api.openweathermap.org/data/2.5/forecast?id=524901&q=London&units=metric&appid=0b5ada9e11785005df4e2b993928203c";
-		$.ajax({
-			url: url,
-			dataType: "jsonp",
-			success : this.parse4hours,
-			error : function(req, err){ console.log('API call failed ' + err); }
-		})
 	}
 
-	
+
+	setToHome = () => {
+		this.setState({home:true})
+		this.setState({dpage:false})
+		this.setState({display2:false})
+		this.setState({display:true})
+	}
+
+	setToDisruption = () => {
+		this.setState({home:false})
+		this.setState({dpage:true})
+		this.setState({display:false})
+		this.setState({display2:true})
+	}
 
 	render() {
 		const tempStyles = this.state.temp ? `${style.temperature} ${style.filled}` : style.temperature;
 	
 		return (
 		<div class={ style.container }>
+			{this.state.home && !this.state.display?
 			<div class={ style.header }>
-			  <div class={ style.temperature }>{ this.state.temp }</div>
-			  <div class={ style.description }>{ this.state.description }</div>
+				<div class={style.city}>{this.state.locate}</div>
+				<div class={style.conditions}>{this.state.cond}</div>
+				<span class={tempStyles}>{this.state.temp}</span>
+				<div class={style.desc}>{this.state.description}</div>
 			</div>
+			: null
+	        }
 			<div class= { style_iphone.container }> 
 				<div class={style_iphone.button}>
-					{this.state.display ? <Button class={style_iphone.button} clickFunction={this.fetchWeatherData} >Display Weather</Button> : null}
+					{this.state.display && this.state.home? <Button class={style_iphone.button} clickFunction={this.fetchWeatherData} >Display Home Weather</Button> : null}
 				</div>
-			  	{ this.state.display2 ? 
+			  	{ this.state.display2 && this.state.dpage ? 
 					<div>
 					  <Button class={ style_iphone.button } clickFunction={ this.fetchDisruptionData }>Display Disruptions</Button>
 					</div>
 					: null 
 			 	}
-			 	{ this.state.disruptions.length > 0 ?
-				<div>
+			 	{ this.state.disruptions.length && this.state.dpage && !this.state.display2> 0 ?
+				<div class={style.header}>
 					<h2>Disruptions:</h2>
 				  	{this.state.disruptions.map((disruption, i) => (
 					<p key={i}>{disruption}</p>
@@ -101,49 +111,31 @@ export default class Iphone extends Component {
 				: null
 			  }
 			</div>
+			<div class={style_iphone.footer}>
+				<table>
+					<td>
+						<Button class={ style_iphone.button } clickFunction={ this.setToHome }>Home Page</Button>
+					</td>
+					<td>
+						<Button class={ style_iphone.button } clickFunction={ this.setToDisruption}>Disruption Page</Button>
+					</td>
+				</table>
+			</div>
 		</div>
 		);
-	}
-
-	parse4hours = (parsed_json) =>{
-		const temperature1 = parsed_json['list']['0']['main']['temp'];
-		const timestamp1 = parsed_json['list']['0']['dt_txt'];
-		const description1 = parsed_json['list']['0']['weather']['0']['description'];
-		const temperature2 =parsed_json['list']['1']['main']['temp'];
-		const timestamp2 = parsed_json['list']['1']['dt_txt'];
-		const description2 = parsed_json['list']['1']['weather']['0']['description'];
-		const temperature3 =parsed_json['list']['2']['main']['temp'];
-		const timestamp3 = parsed_json['list']['2']['dt_txt'];
-		const description3 = parsed_json['list']['2']['weather']['0']['description'];
-		const temperature4 =parsed_json['list']['3']['main']['temp'];
-		const timestamp4 = parsed_json['list']['3']['dt_txt'];
-		const description4 = parsed_json['list']['3']['weather']['0']['description'];
-
-		this.setState({
-			temp1: temperature1,
-			time1: timestamp1,
-			desc1: description1,
-			temp2: temperature2,
-			time2: timestamp2,
-			desc2: description2,
-			temp3: temperature3,
-			time3: timestamp3,
-			desc3: description3,
-			temp4: temperature4,
-			time4: timestamp4,
-			desc4: description4
-		});
 	}
 	
 	
 	  parseResponse = (parsed_json, type) => {
 		if (type === "weather") {
-		  const temp = parsed_json.main.temp;
-		  const description = parsed_json.weather[0].description;
-		  this.setState({
-			temp: temp,
-			description: description
-		  });
+			const location = parsed_json.name;
+		  	const temp = parsed_json.main.temp;
+		  	const description = parsed_json.weather[0].description;
+		  	this.setState({
+				locate: location,
+				temp: temp,
+				description: description
+		  	});
 		}
 	  }
 	}
